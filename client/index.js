46737f83841404a5f26c267c12148c8775174b03
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2021-03-23 20:39:26 -0300

    feat(server): create socket http

--- client/index.js
+++ client/index.js
@@ -1,8 +1,23 @@
+/**
+node index.js \
+    --username user \
+    --room room01 \
+    --hostUri localhost
+ */
+
 import Events from "events";
+
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
 import TerminalController from "./src/terminalController.js";
 
+const [,, ...commands] = process.argv;
+
+const config = CliConfig.parseArguments(commands);
 
 const componentEmitter = new Events();
-const controller = new TerminalController();
-await controller.initializeTable(componentEmitter);
+const socketClient = new SocketClient(config);
+await socketClient.initialize();
+// const controller = new TerminalController();
+// await controller.initializeTable(componentEmitter);
 
---
+++ server/src/controller.js
@@ -0,0 +1,49 @@
+export default class Controller {
+
+    #users = new Map();
+
+    constructor({ socketServer }) {
+        this.socketServer = socketServer;
+    }
+
+    onNewConnection (socket) {
+        const { id } = socket;
+        console.log('connection stablished with', id)
+
+        const userData = { id, socket };
+        this.#updateGlobalUserData(id, userData);
+
+        socket.on('data', this.#onSocketData(id))
+        socket.on('error', this.#onSocketClosed(id))
+        socket.on('end', this.#onSocketClosed(id))
+        // socket.on('error')
+        // socket.on('end')
+
+    }
+
+    #onSocketData(id) {
+        return data => {
+            console.log('onSocketData', data.toString())
+        }
+    }
+
+    #onSocketClosed(id) {
+        return data => {
+            console.log('onSocketClosed', id)
+        }
+    }
+
+    #updateGlobalUserData(socketId, userData) {
+        const users = this.#users;
+        const user = users.get(socketId) ?? {}
+
+        const updatedUserData = {
+            ...user,
+            ...userData
+        }
+
+        users.set(socketId, updatedUserData)
+
+        return users.get(socketId)
+    }
+}
