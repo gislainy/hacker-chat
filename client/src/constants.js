26cf8bf37c9dbfcfc88e59b51437090c53bf86ce
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2021-03-23 18:52:30 -0300

    feat(client) creating interfaces in command line programs with Blessed package and the Builder pattern

---
+++ client/src/constants.js
@@ -0,0 +1,13 @@
+export const constants = {
+    events: {
+        app: {
+            MESSAGE_SEND: 'message:received',
+            MESSAGE_RECEIVED: 'message:received',
+            ACTIVITY_LOG_UPDATED: 'activityLog:updated',
+            STATUS_UPDATED: 'status:updated'
+        },
+        socket: {
+
+        }
+    }
+}
